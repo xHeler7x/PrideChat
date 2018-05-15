d2985a6410948bb3be5519fa87582f25490d764a
Heler <heler7@o2.pl>
2018-05-15 17:51:35 +0200

    Basic project struckture

---
+++ src/main/java/me/kwiatkowsky/PrideChat/PrideChat/PrideChatApplication.java
@@ -0,0 +1,12 @@
+package me.kwiatkowsky.PrideChat.PrideChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrideChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrideChatApplication.class, args);
+	}
+}
